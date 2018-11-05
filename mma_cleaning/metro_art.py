898c758d618dbbb197b109116df2dad9901f01f7
Christopher Chan <christopher.chan2014@gmail.com>
2018-11-04 23:24:14 -0800

    set up new project and organized existing projects

---
+++ mma_cleaning/metro_art.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#https://www.datacamp.com/community/tutorials/pandas-read-csv
+#Need to: specify what to do with missing values, specify dtypes and then read as csv
+
+art = pd.read_csv('https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv')
+
+
+art.head()
+print('hi')
