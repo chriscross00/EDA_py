a4c342b3c88412231b500cb30a5567c0c758e8f7
Christopher Chan <christopher.chan2014@gmail.com>
2018-11-10 01:42:38 -0800

    added a gitignore

--- brit_lib_cleaning/brit_lib_cleaning.py
+++ brit_lib_cleaning/brit_lib_cleaning.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 import matplotlib as plt
 
 
 print('what')
 
 lib = pd.read_csv('BL-Flickr-Images-Book.csv')
-
 print(lib.head())
